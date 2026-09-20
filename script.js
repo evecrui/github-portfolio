2b4b4fead8c5b7f49e1dfc5f5bb94999703ba134
Eve Cruickshank <evelyaccessible@gmail.com>
2026-09-20 17:50:54 +1200

    Got projects FINALLY showing okay!!!

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
 const body = document.body
 const html = document.documentElement
 
@@ -98,3 +113,4 @@ function showSlides(n, no) {
   }
   x[slideIndex[no]-1].style.display = "block";
 } 
+
